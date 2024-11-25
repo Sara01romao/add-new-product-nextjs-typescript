3ef1aba5763967745d736882c4de73740858d8f8
Sara Romao <sara-romao@live.com>
2024-11-24 23:29:09 -0300

    feat: listagem do cliente e criação rota produto add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 import TableProducts from "@/components/table_products";
-import { MdModeEdit } from "react-icons/md";
-import { TbTrash } from "react-icons/tb";
+import Link from "next/link";
+
 
 export default async function Home() {
-
-  
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-         <h1 className="text-blue-500">Home</h1>
+        
+         <h1 className="text-blue-500 font-bold text-2xl">Product List</h1>
+         <Link href="/product/add">Add</Link>
 
          <TableProducts/>
       </main>
--- src/components/table_products.tsx
+++ src/components/table_products.tsx
@@ -1,4 +1,7 @@
 'use client'
+
+import { useEffect, useState } from "react";
+
 type ProductType ={
     id: number;
     title: string;
@@ -8,28 +11,40 @@ type ProductType ={
     image: string;
   }
 
-export default async function TableProducts(){
+export default function TableProducts(){
+     
+    const [products, setProducts] =  useState<ProductType[]>([]);
 
-    const response = await fetch('https://fakestoreapi.com/products?limit=5');
-    const data = await response.json() as ProductType[];
-    console.log(data)
+    useEffect(() =>{
+     async function getProducts(){
+        const response = await fetch('https://fakestoreapi.com/products?limit=5');
+        const data = await response.json() as ProductType[];
+        console.log(data)
 
+        setProducts(data)
+      }
+
+      getProducts()
+
+    },[])
+  
     return(
        
         <table className="table-fixed">
         <thead>
-          <tr>
-            <th>Name</th>
-            <th>Category</th>
-            <th></th>
+          <tr className="border-b">
+            <th className="text-justify p-2">Name</th>
+            <th className="text-justify">Category</th>
+            <th className="text-justify">Action</th>
+            
           </tr>
         </thead>
         <tbody>
           
-          {data.map(product => 
+          {products.map(product => 
             <tr key={product.id} className="border-b">
               <td className="w-56 p-1">{product.title}...</td>
-              <td className="w-36">{product.category}...</td>
+              <td className="w-40">{product.category}...</td>
               <td>
                 <div className="flex gap-4">
                   <button className="flex items-start text-red-600">Remove</button>
